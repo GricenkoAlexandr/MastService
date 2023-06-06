b54a5d37017b5d5454504bde2918f02d9f183695
Alexandr <alexgriko29@gmail.com>
2023-06-06 22:34:11 +0300

    some footer

--- js/script.js
+++ js/script.js
@@ -1,1 +1,8 @@
-console.log('fff');
+
+const articlebtn=document.querySelector('.articlebtn');
+const articlehide=document.querySelector('.section__text_articlehide');
+
+  articlebtn.addEventListener('click', ()=> {
+    articlehide.classList.toggle('hide');
+    console.log('done');
+  })
