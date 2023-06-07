41fc5462bd1f4044f82ab0484bc298fc73615871
Alexandr <alexgriko29@gmail.com>
2023-06-07 23:06:49 +0300

    catmenu almoust done

--- js/script.js
+++ js/script.js
@@ -1,8 +1,27 @@
 
-const articlebtn=document.querySelector('.articlebtn');
-const articlehide=document.querySelector('.section__text_articlehide');
+/* categori menu */
+const headercat=document.querySelector('.bottommenu__cat'),
+      catlist=document.querySelector('.cat__menu');
+
+      headercat.addEventListener('click', ()=> {
+        catlist.classList.toggle('hidemenu');
+      })
+
+
+
+
+
+
+
+
+
+/* text onoff */
+
+const articlebtn=document.querySelector('.articlebtn'),
+      articlehide=document.querySelector('.section__text_articlehide');
 
   articlebtn.addEventListener('click', ()=> {
     articlehide.classList.toggle('hide');
     console.log('done');
   })
+
